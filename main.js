a3b21378b4e7a407ae3d5968519bc1e218a39e95
pcharlot13 <paulcharlot13@gmail.com>
2024-12-18 16:53:26 -0500

    new begnnings for the staffing tool

---
+++ main.js
@@ -0,0 +1,13 @@
+// ...existing code...
+
+// Function to show a modal
+function showModal(modalId) {
+    $(`#${modalId}`).modal('show');
+}
+
+// Function to hide a modal
+function hideModal(modalId) {
+    $(`#${modalId}`).modal('hide');
+}
+
+// ...existing code...
---
+++ modals.js
@@ -0,0 +1,27 @@
+// Example of a uniform modal structure
+const createModal = (id, title, bodyContent) => {
+    return `
+        <div class="modal fade" id="${id}" tabindex="-1" role="dialog" aria-labelledby="${id}Label" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="${id}Label">${title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ${bodyContent}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+// Example usage
+document.body.insertAdjacentHTML('beforeend', createModal('exampleModal', 'Example Modal', '<p>This is an example modal body.</p>'));
