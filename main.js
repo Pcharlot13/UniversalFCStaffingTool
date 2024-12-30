3f047701b5143e3802640e64f37eeab95e7cd1ee
pcharlot13 <paulcharlot13@gmail.com>
2024-12-30 10:52:46 -0500

    Repurposed repository for tool building application

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// ...existing code...
-
-// Function to show a modal
-function showModal(modalId) {
-    $(`#${modalId}`).modal('show');
-}
-
-// Function to hide a modal
-function hideModal(modalId) {
-    $(`#${modalId}`).modal('hide');
-}
-
-// ...existing code...
+document.getElementById('EPPfiles').addEventListener('click', function() {
+    prompt('You clicked this button');
+});
